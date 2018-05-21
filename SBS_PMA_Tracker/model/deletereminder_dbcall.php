6745dfd1d76bb4e9be133cd103fe928e6e6cc3f1
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 11:29:29 -0400

    Adding comments

--- SBS_PMA_Tracker/model/deletereminder_dbcall.php
+++ SBS_PMA_Tracker/model/deletereminder_dbcall.php
@@ -3,7 +3,7 @@
 //no prepared statements because no user input
 session_start();
 include('../controller/checker.php');
-$reminder_id = $_GET['reminder_id'];
+$reminder_id = $_GET['reminder_id'];//grab id of reminder to delete and put in sql statement
 include('dbopen.php');
 
 $sql = "DELETE FROM reminders WHERE reminder_id=$reminder_id";
