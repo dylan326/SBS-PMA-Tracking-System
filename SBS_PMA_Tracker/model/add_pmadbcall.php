f0747de9a99173d22d96a244bfe1aac820e301a9
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 11:16:17 -0400

    Adding comments

--- SBS_PMA_Tracker/model/add_pmadbcall.php
+++ SBS_PMA_Tracker/model/add_pmadbcall.php
@@ -1,13 +1,13 @@
 <?php
 //file to add a new PMA to the system
 include('DatabaseClass.php');
-
+//create object
 $DatabaseClass = new DatabaseClass();
-
+//prevent XXS Attack
 $nameWashed = $DatabaseClass->wash($_POST['name']);
 
 $daysWashed = $DatabaseClass->wash($_POST['days']);
-
+//call add new pma method from database class
 $newPMA_added = $DatabaseClass->add_new($nameWashed, $daysWashed);
 
 if($newPMA_added == true)
@@ -20,33 +20,5 @@ else
 {
     echo "Error adding new pma.  Contact Administrator";
 }
-/*
-//file to add a new PMA to the system
-session_start();
-include('../controller/checker.php');
-include('dbopen.php');
 
-$pma_name = $_POST['name'];
-$pma_name = htmlspecialchars($pma_name, ENT_QUOTES, 'UTF-8');
- 
-$days = $_POST['days'];
-$days = htmlspecialchars($days, ENT_QUOTES, 'UTF-8');
- 
-$sqlpma = "insert into pma (name, days, in_prog, start_date, date_completed) values ( ?, ?, 0, null, 'New PMA')";
-$stmtpma = $conn->prepare($sqlpma);
-$stmtpma->bind_param("ss", $pma_name, $days);
-$stmtpma->execute();
-
-if(!$stmtpma)
-
-{
-    echo "Error in insert of pma". $conn->error;
-}
-
-else {
-    header("refresh:3; url=../ready_pma.php");
-    echo "New client added to ready PMA. Re-directing.... ";
-}
-$stmtpma->close();
-$conn->close();*/
 ?>
