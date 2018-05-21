f054edf8e66328915dd02cffa2444e6ee70f7e40
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 11:18:11 -0400

    Adding comments

--- SBS_PMA_Tracker/model/adminAdd_inspector.php
+++ SBS_PMA_Tracker/model/adminAdd_inspector.php
@@ -2,14 +2,14 @@
 
 include('dbopen.php');
 include('DatabaseClass.php');
+//create object
 $DatabaseClass = new DatabaseClass();
 
+//prevent XXS attack
 $first_name = $DatabaseClass->wash($_POST['first_name']);
 $last_name = $DatabaseClass->wash($_POST['last_name']);
-//$person_type_id = $_POST['person_type_id'];
 
-
-
+//person_type of 4 means inspector and isActive 1 is a boolean saying employee is active
 $sql = "insert into person (person_type_id, first_name, last_name, isActive) values (4,?,?, 1)";
 
 $stmt = $conn->prepare($sql);
