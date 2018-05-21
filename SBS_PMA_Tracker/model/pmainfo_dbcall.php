1de70cd84df8a6380f9afe51ef93945350a679ad
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 11:44:29 -0400

    Adding comments

--- SBS_PMA_Tracker/model/pmainfo_dbcall.php
+++ SBS_PMA_Tracker/model/pmainfo_dbcall.php
@@ -1,10 +1,12 @@
 <?php
-//file to convert db call to JSON.  Large query that joins multiple tables
+//file to convert db call to JSON for the PMA info of in progress PMAs.  Large query that joins multiple tables
 session_start();
 include('../controller/checker.php');
 include('dbopen.php');
+
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
+//statement pulls all infor PMA manager needs for a PMA that is currently in progress
 $result = $conn->query(" select pma.name, pma.pma_id, person.first_name, person.last_name, person_type.person_type_desc,
 phone.phone, phone_type.phone_type_desc, email.email, email_type.email_type_desc from pma
 inner join pma_person on pma_person.pma_id = pma.pma_id 
