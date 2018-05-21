b1d821370235c6d629e7e7d9c27239864b3087f2
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 11:41:43 -0400

    Adding  comments

--- SBS_PMA_Tracker/model/inspectorLogProcess.php
+++ SBS_PMA_Tracker/model/inspectorLogProcess.php
@@ -1,27 +1,18 @@
 <?php
 include('dbopen.php');
 include('DatabaseClass.php');
-
+//create object
 $DatabaseClass = new DatabaseClass();
-
+//set post varible.  Hidden value passed so no need to wash
 $inspector_id = $_POST['inspector'];
-
-
-
+//prevent XXS Attack
 $building = $DatabaseClass->wash($_POST['building']);
-
 $description = $DatabaseClass->wash($_POST['description']);
-
 $floors  = $DatabaseClass->wash($_POST['floors']);
-
-
 $issues  = $DatabaseClass->wash($_POST['issues']);
-
-
 $days  = $DatabaseClass->wash($_POST['days']);
 
-
-
+//prepared statement to insert into daily logs.  Had to covert timezone to Eastern standard time. 
 $sql = "insert into daily_logs (person_id, building, description, floors, issues, days_left, date_time) values (?,?,?,?,?,?,CONVERT_TZ(NOW(),'+00:00', '-04:00'))";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("isssss", $inspector_id, $building, $description, $floors, $issues, $days);
